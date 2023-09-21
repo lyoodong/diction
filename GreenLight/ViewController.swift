82a0dbf6e7cfc69e2ea57eddc85493ec40842380
lyoodong <dwryu3079@naver.com>
2023-09-21 16:18:34 +0900

    Initial Commit

---
+++ GreenLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenLight
+//
+//  Created by Dongwan Ryoo on 2023/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
