0fc06a10531b2013f8b4e87549cfd6fe10bdabd6
lyoodong <dwryu3079@naver.com>
2023-09-25 21:08:28 +0900

    [ADD] : #4 CRUDType(읽기, 필터/ 추가, 업데이트) 추가

---
+++ GreenLight/Source/Presenter/Gloabl/Enum/CRUDType.swift
@@ -0,0 +1,18 @@
+//
+//  CRUDType.swift
+//  GreenLight
+//
+//  Created by Dongwan Ryoo on 2023/09/25.
+//
+
+import Foundation
+
+enum ReadType {
+    case read
+    case sort
+}
+
+enum WriteType {
+    case add
+    case update
+}
