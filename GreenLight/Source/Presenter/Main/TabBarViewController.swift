ea6ab342ac75a92c2b301e7cfeb3bf05d8e0db7a
lyoodong <dwryu3079@naver.com>
2023-10-01 16:20:17 +0900

    [ADD]: #15 탭바 랜덤탭 추가

--- GreenLight/Source/Presenter/Main/TabBarViewController.swift
+++ GreenLight/Source/Presenter/Main/TabBarViewController.swift
@@ -6,25 +6,35 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
-class TabBarViewController: UITabBarController {
+final class TabBarViewController: UITabBarController {
     
     //추가할 VC
-    let main = MainViewController()
-    let myPage = MyPageViewController()
+    private let main = MainViewController()
+    private let myPage = MyPageViewController()
+    
+    private let randomTap = UIButton().then {
+        $0.setTitle("랜덤", for: .normal)
+        $0.backgroundColor = .mainBlue
+        $0.layer.cornerRadius = 28
+        $0.addTarget(self, action: #selector(randomTapAddTarget), for: .touchUpInside)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
         viewSet()
+        randomTapConstraints()
     }
     
     //탭바 설정
     func viewSet(){
         addViewControllers()
-        self.tabBar.roundCorners(cornerRadius: 25, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        self.tabBar.roundCorners(cornerRadius: 18, maskedCorners: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
         self.tabBar.tintColor = .mainBlue
-        self.tabBar.unselectedItemTintColor = .bgGrey
+        self.tabBar.unselectedItemTintColor = .tabBarGrey
         self.tabBar.backgroundColor = .white
     }
     
@@ -43,5 +53,21 @@ class TabBarViewController: UITabBarController {
         nav.tabBarItem.title = title
 
         return nav
+    }
+    
+    func randomTapConstraints() {
+        view.addSubview(randomTap)
+        
+        randomTap.snp.makeConstraints {
+            $0.size.equalTo(56)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(tabBar.snp.top).inset(15)
+        }
+    }
+    
+    @objc
+    func randomTapAddTarget() {
+        let vc = RandomViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
