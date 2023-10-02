274d214231869dfbe9411a532b69e86d3cc0a702
lyoodong <dwryu3079@naver.com>
2023-10-02 18:08:45 +0900

    [ADD]: MainView 기본 UI 추가

---
+++ GreenLight/Source/Presenter/Main/MainView.swift
@@ -0,0 +1,31 @@
+//
+//  MainView.swift
+//  GreenLight
+//
+//  Created by Dongwan Ryoo on 2023/10/01.
+//
+
+import UIKit
+
+class MainView: BaseView {
+    
+    lazy var mainCollectionView = BaseCollectionView()
+    
+    override func viewSet() {
+        self.backgroundColor = .mainBlue
+        addSubView()
+    }
+    
+    func addSubView() {
+        [mainCollectionView].forEach(addSubview)
+    }
+    
+    override func constraints() {
+        mainCollectionView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.left.equalTo(self.safeAreaLayoutGuide)
+            $0.right.equalTo(self.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
--- GreenLight/Source/Presenter/Main/MainViewController.swift
+++ GreenLight/Source/Presenter/Main/MainViewController.swift
@@ -9,4 +9,81 @@ import UIKit
 
 class MainViewController: BaseViewController {
     
+    let mainView = MainView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func viewSet() {
+        setNavigationItem()
+        setCollectionView()
+    }
+    
+    override func constraints() {
+        
+    }
+    
 }
+
+// MARK: - setNavigationItem
+extension MainViewController {
+    
+    func setNavigationItem() {
+        navigationItem.title = "나의 질문"
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.barStyle = .default
+        
+        
+        let logoImage = UIImage(systemName: "star.fill")
+        let logoBarButton = UIBarButtonItem(image: logoImage, style: .plain, target: self, action: nil)
+        navigationItem.leftBarButtonItem = logoBarButton
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        navigationItem.rightBarButtonItem = addButton
+        
+        let searchBarController = BaseSearchController()
+        searchBarController.searchBar.delegate = self
+        searchBarController.searchBar.searchTextField.backgroundColor = .white
+        searchBarController.searchBar.searchTextField.placeholder = " 질문을 검색해 보세요"
+        navigationItem.searchController = searchBarController
+    }
+    
+}
+
+// MARK: - UISearchBarDelegate
+extension MainViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        
+    }
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func setCollectionView() {
+        mainView.mainCollectionView.delegate = self
+        mainView.mainCollectionView.dataSource = self
+        mainView.mainCollectionView.register(BaseCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? BaseCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+
+}
+
+
+
