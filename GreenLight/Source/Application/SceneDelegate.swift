ed620e4e498df700192d82cc6f0ce300336f993a
lyoodong <dwryu3079@naver.com>
2023-10-03 10:35:55 +0900

    [DEL]: #27 탭바의 네비게이션 컨트롤러 삭제

--- GreenLight/Source/Application/SceneDelegate.swift
+++ GreenLight/Source/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let vc = TabBarViewController()
-        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
     }
 
