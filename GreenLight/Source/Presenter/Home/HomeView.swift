ba7e9ece1fa7b9f1ea1eae9df4b4c28b7226862e
lyoodong <dwryu3079@naver.com>
2023-10-03 10:44:20 +0900

    [COMMENT]: #33 불필요한 주석 제거

--- GreenLight/Source/Presenter/Home/HomeView.swift
+++ GreenLight/Source/Presenter/Home/HomeView.swift
@@ -43,12 +43,3 @@ final class HomeView: BaseView {
     }
 }
 
-
-//func sortButtonSet() {
-//    let favorite = UIAction(title: "평점입력", image: UIImage(systemName: "star"), handler: { _ in print("평점입력") })
-//    sortButton.menu = UIMenu(title: "타이틀",
-//                         image: UIImage(systemName: "heart"),
-//                         identifier: nil,
-//                         options: .displayInline,
-//                         children: [favorite])
-//}
