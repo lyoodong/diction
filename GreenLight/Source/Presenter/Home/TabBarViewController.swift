cce1cd35fdeb7e78939ab0e29785e2f2ea753322
lyoodong <dwryu3079@naver.com>
2023-10-04 15:13:56 +0900

    [DESIGN]:  #37 랜덤 탭 UI 변경

--- GreenLight/Source/Presenter/Home/TabBarViewController.swift
+++ GreenLight/Source/Presenter/Home/TabBarViewController.swift
@@ -13,13 +13,8 @@ final class TabBarViewController: UITabBarController {
     
     private let home = HomeViewController()
     private let myPage = MyPageViewController()
-    
-    private let randomTap = UIButton().then {
-        $0.setTitle("랜덤", for: .normal)
-        $0.backgroundColor = .mainBlue
-        $0.layer.cornerRadius = 28
-        $0.addTarget(self, action: #selector(randomTapAddTarget), for: .touchUpInside)
-    }
+    private let random = RandomViewController()
+    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +24,6 @@ final class TabBarViewController: UITabBarController {
     func viewSet(){
         setTabBar()
         addViewControllers()
-        randomTapLayout()
     }
     
     func setTabBar() {
@@ -38,14 +32,16 @@ final class TabBarViewController: UITabBarController {
         self.tabBar.unselectedItemTintColor = .tabBarGrey
         self.tabBar.backgroundColor = .white
         self.tabBar.isTranslucent = true
+        self.delegate = self
     }
     
     func addViewControllers() {
         let homeTab = createViewController(title: "내 질문", imageName: "headphones", viewController: home)
+        let randomTab = createViewController(title: "랜덤 면접", imageName: "dice", viewController: myPage)
         let myPageTab = createViewController(title: "내 정보", imageName: "person.fill", viewController: myPage)
-        self.viewControllers = [homeTab, myPageTab]
+        self.viewControllers = [homeTab, randomTab, myPageTab]
     }
-        
+    
     func createViewController(title: String, imageName: String, viewController: UIViewController) -> UIViewController {
         let nav = UINavigationController(rootViewController: viewController)
         let image = UIImage(systemName:imageName)
@@ -54,21 +50,12 @@ final class TabBarViewController: UITabBarController {
         
         return nav
     }
+}
+
+extension TabBarViewController: UITabBarControllerDelegate {
     
-    func randomTapLayout() {
-        view.addSubview(randomTap)
-        
-        randomTap.snp.makeConstraints {
-            $0.size.equalTo(56)
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalTo(tabBar.snp.top).inset(15)
-        }
-    }
-    
-    @objc
-    func randomTapAddTarget() {
-        let vc = RandomViewController()
-        let nav = UINavigationController(rootViewController: vc)
-        self.navigationController?.pushViewController(vc, animated: true)
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        self.feedbackGenerator.prepare()
+        self.feedbackGenerator.impactOccurred()
     }
 }
